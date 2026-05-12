70275c46085c0ae901e2ca020afbf90c303ce6e3
Erniexcs <134022036+Erniexcs@users.noreply.github.com>
2026-05-12 12:30:42 -0500

    implemento chatia

---
+++ src/main/java/com/upc/perulisto/DTO/ChatMensajeResponse.java
@@ -0,0 +1,18 @@
+package com.upc.perulisto.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMensajeResponse {
+
+    private String respuesta;
+    private LocalDateTime timestamp;
+}
